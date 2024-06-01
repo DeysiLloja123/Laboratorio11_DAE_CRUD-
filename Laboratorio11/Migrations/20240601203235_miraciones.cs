b54805829e9b140ad1ae948c396170d3eee755f0
Alumno <Alumno@LAB1504-25>
2024-06-01 15:59:11 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio11/Migrations/20240601203235_miraciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laboratorio11.Migrations
+{
+    /// <inheritdoc />
+    public partial class miraciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Enrollments",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Enrollments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
---
+++ Laboratorio11/Models/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Collections.Generic;
+
+namespace Laboratorio11.Models
+{
+    public class SchoolContext:DbContext
+
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAB1504-25\\SQLEXPRESS;"+
+                "Initial Catalog=semana11; ; trustservercertificate=True;User Id=UserTecsup;Password=UserTecsup");
+        }
+
+    }
+    
+}
